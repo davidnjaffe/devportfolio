6ef54bff02f238430559cccddde80e337459f75d
David <Jaffe>
2021-08-12 21:57:59 -0700

    footer update

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -12,7 +12,7 @@ export default class Footer extends Component {
             <footer className="footer mt-auto py-3 col-md-12" id="sticky-footer">
             <span class="footer-text"><a href="mailto:davidnjaffe@gmail.com">davidnjaffe@gmail.com</a> | <a href="https://www.linkedin.com/in/davidnjaffe/" target="blank">LinkedIn</a> | <a href="https://github.com/davidnjaffe" target="blank">GitHub</a></span>
             <br></br>
-            <p>©2020 David Jaffe</p>
+            <p>©2021 David Jaffe</p>
             
               
             
